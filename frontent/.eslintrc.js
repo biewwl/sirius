f44b7d2063ff28c0adc2c94d4f9d01a6632419ee
Gabriel Dias <gabriel.dias.fern@gmail.com>
2023-02-11 13:58:43 -0300

    FEAT: Create file app_config

---
+++ frontent/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
