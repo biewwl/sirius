4352409bdd0e8c980ba6a1acd3e9cd65f5112e42
Gabriel Dias <gabriel.dias.fern@gmail.com>
2023-02-12 00:12:15 -0300

    FEAT: Implements Register on frontend

---
+++ frontent/src/helpers/schemas/registerJoi.js
@@ -0,0 +1,38 @@
+import Joi from "joi";
+
+import appConfig from "../../app_config.json";
+const errorMessages =
+  appConfig["app.components"]["LoginRegister"]["register"]["error.messages"];
+
+const schema = Joi.object({
+  name: Joi.string().min(3).max(30).required().messages({
+    "string.min": errorMessages["name.small"],
+    "string.max": errorMessages["name.big"],
+    "string.required": errorMessages["name.required"],
+  }),
+  nick: Joi.string().min(3).max(20).required().messages({
+    "string.min": errorMessages["nick.small"],
+    "string.max": errorMessages["nick.big"],
+    "string.required": errorMessages["nick.required"],
+  }),
+  email: Joi.string()
+    .min(3)
+    .max(30)
+    .regex(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/)
+    .required()
+    .messages({
+      "string.min": errorMessages["email.small"],
+      "string.max": errorMessages["email.big"],
+      "string.required": errorMessages["email.required"],
+      "string.pattern.base": errorMessages["email.format"],
+    }),
+  password: Joi.string().min(8).max(50).required().messages({
+    "string.min": errorMessages["password.small"],
+    "string.max": errorMessages["password.big"],
+    "string.required": errorMessages["password.required"],
+  }),
+});
+
+const validateRegister = (registerData) => schema.validate(registerData);
+
+export default validateRegister;
